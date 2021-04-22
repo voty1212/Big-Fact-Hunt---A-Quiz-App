4fc937d3b518fd9f6c070898a701fddfd6b1a49a
munjanishubham <66631668+munjanishubham@users.noreply.github.com>
2021-04-22 22:54:29 +0530

    created viewholder and bindviewholder in "categoryAdapter.java"

--- app/src/main/java/com/example/quizus/categoryAdapter.java
+++ app/src/main/java/com/example/quizus/categoryAdapter.java
@@ -1,13 +1,45 @@
 package com.example.quizus;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class categoryAdapter {
+import java.util.ArrayList;
+
+public class categoryAdapter extends RecyclerView.Adapter<categoryAdapter.categoryViewHolder> {
+
+    Context context;
+    ArrayList<categoryModel> categoryModels;
+    public categoryAdapter(Context context, ArrayList<categoryModel> categoryModels){
+
+        this.context = context;
+        this.categoryModels = categoryModels;
+    }
+
+    @NonNull
+    @Override
+    public categoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_category,null);
+        return categoryViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull categoryViewHolder holder, int position) {
+        categoryModel model = categoryModels.get(position);
+
+        holder.textView.setText(model.getCategoryName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return categoryModels.size();
+    }
 
     public class categoryViewHolder extends RecyclerView.ViewHolder{
 
