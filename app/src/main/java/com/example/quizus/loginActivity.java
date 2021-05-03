fa09a0d22bff77e0c3f1d4ea759a2cd7adc10fc9
munjanishubham <66631668+munjanishubham@users.noreply.github.com>
2021-05-03 18:49:58 +0530

    linked login with firebase

--- app/src/main/java/com/example/quizus/loginActivity.java
+++ app/src/main/java/com/example/quizus/loginActivity.java
@@ -1,14 +1,56 @@
 package com.example.quizus;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.quizus.databinding.ActivityLoginBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class loginActivity extends AppCompatActivity {
+
+    ActivityLoginBinding binding;
+    FirebaseAuth auth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        auth = FirebaseAuth.getInstance();
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email,pass;
+
+                email = binding.emailbox.getText().toString();
+                pass = binding.passwordbox.getText().toString();
+
+                auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+
+                            Toast.makeText(loginActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            Toast.makeText(loginActivity.this, task.getException().getLocalizedMessage() , Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
+
     }
 }
