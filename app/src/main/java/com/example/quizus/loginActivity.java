ad1bfc17978d243e72794339b5bfd2b729366b46
munjanishubham <66631668+munjanishubham@users.noreply.github.com>
2021-05-04 16:38:00 +0530

    linked signup and login screen

--- app/src/main/java/com/example/quizus/loginActivity.java
+++ app/src/main/java/com/example/quizus/loginActivity.java
@@ -3,6 +3,7 @@ package com.example.quizus;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -49,6 +50,13 @@ public class loginActivity extends AppCompatActivity {
                     }
                 });
 
+            }
+        });
+
+        binding.createAccBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(loginActivity.this, signup.class));
             }
         });
 
--- app/src/main/java/com/example/quizus/signup.java
+++ app/src/main/java/com/example/quizus/signup.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -54,5 +55,13 @@ public class signup extends AppCompatActivity {
 
             }
         });
+
+        binding.alreadyaccountbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(signup.this, loginActivity.class));
+            }
+        });
+
     }
 }
