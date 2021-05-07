f91d31f9e4bfff86f316fe22804dc3b4cd6df9cc
munjanishubham <66631668+munjanishubham@users.noreply.github.com>
2021-05-07 21:36:26 +0530

    linked quiz layout with category
    when you click any category, you will be directed to quiz layout

--- app/src/main/java/com/example/quizus/categoryAdapter.java
+++ app/src/main/java/com/example/quizus/categoryAdapter.java
@@ -1,6 +1,7 @@
 package com.example.quizus;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -34,6 +35,16 @@ public class categoryAdapter extends RecyclerView.Adapter<categoryAdapter.catego
         categoryModel model = categoryModels.get(position);
 
         holder.textView.setText(model.getCategoryName());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, quizActivity.class);
+                intent.putExtra("catId", model.getCategoryId());
+                context.startActivity(intent);
+            }
+        });
+
     }
 
     @Override
