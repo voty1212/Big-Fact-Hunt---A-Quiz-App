fca67a5b11190d118631d02e6e5e3a3e530e148c
Srinivasan R Bashyam <srinivasanbashyam2001@gmail.com>
2021-05-09 02:22:49 +0530

    Changed class name, Added timer and question counter, removed some errors

--- app/src/main/java/com/example/quizus/question.java
+++ app/src/main/java/com/example/quizus/question.java
@@ -1,13 +1,13 @@
 package com.example.quizus;
 
-public class question {
+public class Question {
 
     private String question, option1, option2, option3, option4, answer;
 
-    public question() {
+    public Question() {
     }
 
-    public question(String question, String option1, String option2, String option3, String option4, String answer) {
+    public Question(String question, String option1, String option2, String option3, String option4, String answer) {
         this.question = question;
         this.option1 = option1;
         this.option2 = option2;
--- app/src/main/java/com/example/quizus/quizActivity.java
+++ app/src/main/java/com/example/quizus/quizActivity.java
@@ -3,7 +3,9 @@ package com.example.quizus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.quizus.databinding.ActivityQuizBinding;
@@ -14,9 +16,11 @@ public class quizActivity extends AppCompatActivity {
 
     ActivityQuizBinding binding;
 
-    ArrayList<question> questions;
+    ArrayList<Question> questions;
 
     int index = 0;
+    Question question;
+    CountDownTimer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,21 +30,72 @@ public class quizActivity extends AppCompatActivity {
 
         questions = new ArrayList<>();
 
-        questions.add(new question("What is earth?", "Planet", "car", "bike", "book", "Planet"));
-        questions.add(new question("What is samosha?", "Planet", "car", "bike", "Food", "Food"));
+        questions.add(new Question("What is earth?", "Planet", "car", "bike", "book", "Planet"));
+        questions.add(new Question("What is samosa?", "Planet", "car", "bike", "Food", "Food"));
 
         setNextQuestion();
+        reset_timer();
+    }
+    void reset_timer(){
+        timer=new CountDownTimer(30000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                binding.timer.setText(String.valueOf(millisUntilFinished/1000));
+
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        };
     }
 
     void setNextQuestion(){
+        reset_timer();
+        if(timer !=null)
+            timer.cancel();
+        timer.start();
         if(index < questions.size()){
-            question question = questions.get(index);
+            binding.questionCounter.setText(String.format("%d/%d",(index+1),(questions.size())));
+            question = questions.get(index);
             binding.question.setText(question.getQuestion());
             binding.option1.setText(question.getOption1());
             binding.option2.setText(question.getOption2());
             binding.option3.setText(question.getOption3());
             binding.option4.setText(question.getOption4());
         }
+    }
+    void show_answer(){
+        if(question.getAnswer().equals(binding.option1.getText().toString()))
+            binding.option1.setBackground(getResources().getDrawable(R.drawable.option_right));
+        else if(question.getAnswer().equals(binding.option2.getText().toString()))
+            binding.option2.setBackground(getResources().getDrawable(R.drawable.option_right));
+        else if(question.getAnswer().equals(binding.option3.getText().toString()))
+            binding.option3.setBackground(getResources().getDrawable(R.drawable.option_right));
+        else if(question.getAnswer().equals(binding.option4.getText().toString()))
+            binding.option4.setBackground(getResources().getDrawable(R.drawable.option_right));
+
+
+    }
+    void check_answer(TextView textview){
+        String Selected_ans= textview.getText().toString();
+        if(Selected_ans.equals(question.getAnswer()))
+        {
+            textview.setBackground(getResources().getDrawable(R.drawable.option_right));
+        }
+        else
+        {   show_answer();
+            textview.setBackground(getResources().getDrawable(R.drawable.option_wrong));
+        }
+
+    }
+    void reset(){
+        binding.option1.setBackground(getResources().getDrawable(R.drawable.option_unselected));
+        binding.option2.setBackground(getResources().getDrawable(R.drawable.option_unselected));
+        binding.option3.setBackground(getResources().getDrawable(R.drawable.option_unselected));
+        binding.option4.setBackground(getResources().getDrawable(R.drawable.option_unselected));
+
     }
 
     public void onClick(View view ){
@@ -50,11 +105,17 @@ public class quizActivity extends AppCompatActivity {
             case R.id.option_2:
             case R.id.option_3:
             case R.id.option_4:
+                if(timer!=null)
+                    timer.cancel();
+                TextView selected=(TextView) view;
+                check_answer(selected);
+
                 Toast.makeText(this, "Option Clicked", Toast.LENGTH_SHORT).show();
                 break;
 
 
             case R.id.nextBtn:
+                reset();
                 if(index < questions.size()) {
                     index++;
                     setNextQuestion();
